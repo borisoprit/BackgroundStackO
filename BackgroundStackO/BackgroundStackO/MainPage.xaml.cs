55fd3e8b35ed361c777318a013972b0055d82ed1
bashobbel <bashobbel@gmail.com>
2022-04-17 18:18:59 +0200

    Add project files.

---
+++ BackgroundStackO/BackgroundStackO/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace BackgroundStackO
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            App.Current.Resources["defaultBackgroundColor"] = Preferences.Get("BackgroundColor", "Blue");
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Preferences.Set("BackgroundColor", "Green");
+            App.Current.Resources["defaultBackgroundColor"] = Preferences.Get("BackgroundColor", "Blue");
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            Preferences.Set("BackgroundColor", "Red");
+            App.Current.Resources["defaultBackgroundColor"] = Preferences.Get("BackgroundColor", "Blue");
+        }
+    }
+}
